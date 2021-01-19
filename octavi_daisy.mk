141436d2fa54009d4a7127dbc1fd6144f7f020d0
100Daisy <100Daisy@protonmail.com>
2021-01-19 11:02:05 +0000

    OctaviOS-ify

---
+++ octavi_daisy.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from daisy device
+$(call inherit-product, device/xiaomi/daisy/device.mk)
+
+# Inherit some common OctaviOS sauce.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+# Adding OctaviOS stuff
+OCTAVI_BUILD_TYPE := Official
+OCTAVI_DEVICE_MAINTAINER := GitDaisy
+#TARGET_USES_BLUR=true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := daisy
+PRODUCT_NAME := octavi_daisy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi Mi A2 Lite
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sunfish-user 11 RQ1A.201205.008 6943376 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ1A.201205.008/6943376:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
